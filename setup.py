bb5761cf1cfba930f5961fb806b282698103ce45
MPodolak <mati.pooo@gmail.com>
2021-01-06 14:54:33 +0100

    Test project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Project setup."""
+#!/usr/bin/env python
+# 3rd-party
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='car_rating',
+      version='1.0',
+      packages=find_packages())
