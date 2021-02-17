a1fa7ccef8d4fc4ca954f93b1e42aa08609390f3
MPodolak <mati.pooo@gmail.com>
2021-02-17 21:32:10 +0100

    changes

--- car_rating/settings.py
+++ car_rating/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'car_rating.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
