b5ba07c204cb8859209c3debe2e217092ad6ee49
MPodolak <mati.pooo@gmail.com>
2021-02-17 21:25:40 +0100

    changes

--- car_rating/settings.py
+++ car_rating/settings.py
@@ -14,6 +14,7 @@ import os
 
 # 3rd-party
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,6 +130,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 try:
     # Project
